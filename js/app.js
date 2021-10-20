343165a0ae94ae10e6592d239af25acf4c479158
emma <emma.ronaldo7@gmail.com>
2021-10-20 10:52:14 -0400

    add eventListener

---
+++ js/app.js
@@ -0,0 +1,25 @@
+/*----------------- Constants -----------------*/
+
+
+
+/*------------- Variables (state) -------------*/
+
+
+
+/*--------- Cached Element References ---------*/
+
+const initialCount = document.querySelector('#initial-count');
+const input = document.querySelector('#user-input');
+const plusBtn = document.querySelector('#plus');
+const minusBtn = document.querySelector('#minus');
+
+/*-------------- Event Listeners --------------*/
+plusBtn.addEventListener('click', () =>{
+  console.log('plus Btn clicked')
+})
+minusBtn.addEventListener('click', () =>{
+  console.log('minus Btn clicked')
+})
+
+
+/*----------------- Functions -----------------*/
